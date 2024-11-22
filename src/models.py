21eacde54f08f5d70b83fe2daee550b0f8d87c9e
Ana <sm.vulinana@gmail.com>
2024-11-23 00:09:36 +0100

    added missing package

--- src/models.py
+++ src/models.py
@@ -6,6 +6,6 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
-    
+
     def __repr__(self):
         return f'<User {self.username}>'
