1eb7a56688e1c57d9c67ac5334e84030c6c8aed0
Ana <sm.vulinana@gmail.com>
2024-11-23 00:36:41 +0100

    quick fix

--- src/main.py
+++ src/main.py
@@ -1,24 +1,24 @@
 from flask import Flask
-
 from src.extensions import socketio, db, migrate
 from src.config import Config
 from flask_cors import CORS
 
+app = Flask(__name__)
+app.config.from_object(Config)
+
+socketio.init_app(app)
+db.init_app(app)
+migrate.init_app(app, db)
+
+with app.app_context():
+    from .models import User
+    db.create_all()
+
+from .gme.routes import gme_routes
+app.register_blueprint(gme_routes)
+
+CORS(app)
+
+# Ovo je za kada pokrećeš aplikaciju lokalno sa Flask-om.
 if __name__ == "__main__":
-    app = Flask(__name__)
-    app.config.from_object(Config)
-
-    socketio.init_app(app)
-    db.init_app(app)
-    migrate.init_app(app, db)
-
-    with app.app_context():
-        from .models import User
-        db.create_all()
-
-    from .gme.routes import gme_routes
-    app.register_blueprint(gme_routes)
-
-
-    CORS(app)
     socketio.run(app, allow_unsafe_werkzeug=True)
