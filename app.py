2d7680298649bc46ac1f9d21a31d45c3cefef43d
Ana <sm.vulinana@gmail.com>
2024-11-24 14:47:12 +0100

    .

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask
 from extensions import socketio, db, migrate
 from config import Config
 from flask_cors import CORS
 from gme.routes import gme_routes
 import os
-import eventlet
-
-eventlet.monkey_patch()
 
 app = Flask(__name__)
 app.config.from_object(Config)
