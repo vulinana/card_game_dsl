255f0814fcec1fcc578c3fdf08bcb8383e941921
Ana <sm.vulinana@gmail.com>
2024-11-24 12:39:18 +0100

    changed to postgresql

--- config.py
+++ config.py
@@ -1,12 +1,9 @@
 import os
 
 class Config:
+    # Konektovanje sa PostgreSQL bazom koristeći Railway URL
     SQLALCHEMY_DATABASE_URI = (
-        'mssql+pyodbc://jsd:MaloAzura22@jsd-dbserver.database.windows.net/jsd'
-        '?driver=ODBC+Driver+17+for+SQL+Server'
-        '&Encrypt=yes'
-        '&TrustServerCertificate=no'
-        '&ConnectionTimeout=30'
+        'postgresql://postgres:myhNAiNTJAltMmxjBOFatbshqNSChyyI@autorack.proxy.rlwy.net:23481/railway'
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'my_secret_key')  # Koristite env varijablu za tajni ključ
