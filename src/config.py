af99b270bb9829c8de4b79e0a84d2e3372ce21d1
Ana <sm.vulinana@gmail.com>
2024-11-24 11:26:31 +0100

    fixed connection string

--- src/config.py
+++ src/config.py
@@ -4,6 +4,9 @@ class Config:
     SQLALCHEMY_DATABASE_URI = (
         'mssql+pyodbc://jsd:MaloAzura22@jsd-dbserver.database.windows.net/jsd'
         '?driver=ODBC+Driver+17+for+SQL+Server'
+        '&Encrypt=yes'
+        '&TrustServerCertificate=no'
+        '&ConnectionTimeout=30'
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'my_secret_key')  # Koristite env varijablu za tajni ključ
